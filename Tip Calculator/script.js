a5a013271c72459a3b1fc928a90fe7cc60a109df
Nehareddy1711 <nehaatham@lyrostech.com>
2024-04-03 12:54:17 +0530

    commit

--- Tip Calculator/script.js
+++ Tip Calculator/script.js
@@ -2,15 +2,6 @@ let input_box=document.querySelector("input");
 let btn=document.querySelector("button");
 let output=document.querySelector(".output")
 
-//once button is clicked
-//get the value of input box
-//and find 2% as tip
-//show the output
-
-
-//event=>click
-//addevent listener()
-
 btn.addEventListener("click",calculateTip);
 
 function calculateTip(){
